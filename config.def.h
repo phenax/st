645267de897c0344a752a07c7d22d039de72acff
Akshay Nair <phenax5@gmail.com>
2020-03-22 18:25:55 +0530

    Adds open url from output with rofi menu

--- config.def.h
+++ config.def.h
@@ -192,6 +192,10 @@ static MouseShortcut mshortcuts[] = {
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
 
+static char *openurlcmd[] = { "/bin/sh", "-c",
+	"xurls | rofi -dmenu -p 'URLs' -l 10 -w $WINDOWID -theme dmenu | xargs -r xdg-open",
+	"externalpipe", NULL };
+
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
@@ -210,7 +214,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-    { TERMMOD,              XK_Escape,      keyboard_select,{ 0 } },
+  { TERMMOD,              XK_Escape,      keyboard_select, { 0 } },
+  { TERMMOD, XK_U, externalpipe, { .v = openurlcmd } },
 };
 
 /*
