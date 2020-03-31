182f8de863245848e5d78348b7560f40433d6d5b
Akshay Nair <phenax5@gmail.com>
2020-04-01 02:53:44 +0530

    tempt

--- config.def.h
+++ config.def.h
@@ -226,9 +226,6 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	// { XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	// { XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	// { XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 	// { XK_ANY_MOD,              Button4,     kscrollup,        {.i = scrollincrement} },
 	// { XK_ANY_MOD,              Button5,   kscrolldown,      {.i = scrollincrement} },
 };
