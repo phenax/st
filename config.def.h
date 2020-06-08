b2f1f8a91b31e2c49109f01b855acb4ebffbe615
Akshay Nair <phenax5@gmail.com>
2020-06-08 16:07:35 +0530

    Changes font to jetbrains

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
   "Font Awesome 5 Free:pixelsize=12:antialias=true:autohint=true",
