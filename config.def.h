e7c2713c18d0860fff7d53d31dc0a9a1a08ea629
Akshay Nair <phenax5@gmail.com>
2020-12-21 14:27:17 +0530

    Changes font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
   "CozetteVector:pixelsize=12:antialias=true:autohint=true",
